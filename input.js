5de3ed4e8b980df45d37600e7f2551833ee36cea
Nanaya Miki <nanayamiki@gmail.com>
2023-04-21 21:25:21 +0000

    added WASD movements

--- input.js
+++ input.js
@@ -1,20 +1,33 @@
+let connection;
+
 const handleUserInput = function (key) {
 	// \u0003 maps to ctrl+c input
 
 	if (key === "\u0003") {
 		process.exit();
 	}
+	if (key === "w") {
+		connection.write("Move: up");
+	}
+	if (key === "a") {
+		connection.write("Move: left");
+	}
+	if (key === "s") {
+		connection.write("Move: down");
+	}
+	if (key === "d") {
+		connection.write("Move: right");
+	}
 };
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
 	const stdin = process.stdin;
+	connection = conn; //connect()
 	stdin.setRawMode(true);
 	stdin.setEncoding("utf8");
 	stdin.resume();
 	stdin.on("data", handleUserInput);
 	return stdin;
 };
-
-setupInput();
 
 module.exports = { setupInput };
